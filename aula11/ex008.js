7452c179f810025cb86b4e101c3eaea3fe0da2dc
Tiago <116377137+xeytak@users.noreply.github.com>
2022-12-03 10:02:17 -0300

    condições 1

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 61
+console.log(`A velocidade do seu veículo é de ${vel} Km/h`)
+if (vel > 60){
+    console.log(`Você ultrapassou os limites de velocidade: ${vel}Km/h. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+
+// CONDIÇÃO SIMPLES !
---
+++ aula11/ex009.js
@@ -0,0 +1,11 @@
+var país = 'EUA'
+console.log(`Você nasceu em ${país}`)
+
+if (país == 'BRASIL'){
+    console.log(`BRASILEIRO`)
+}
+
+else{
+    console.log(`ESTRANGEIRO!`)
+}
+    
