e65a1070a3a5a6125e76273ed147ffe30f720027
Tiago <116377137+xeytak@users.noreply.github.com>
2022-12-04 15:54:19 -0300

    Create while.js

---
+++ aula13/while.js
@@ -0,0 +1,16 @@
+
+// USANDO O WHILE:
+
+var c = 1 
+while (c <= 6){
+    console.log('Tiago!')
+    c += 1
+}
+
+// USANDO O DO:
+
+var c2 = 1
+do{
+    console.log('Tiago 2')
+    c2 += 1
+} while (c2 <= 6)
