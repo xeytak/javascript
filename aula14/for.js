10de802339af8bb85fde43706367f6739efe8a25
Tiago <116377137+xeytak@users.noreply.github.com>
2022-12-05 09:48:49 -0300

    aula 14

---
+++ aula14/for.js
@@ -0,0 +1,17 @@
+/* 
+
+var = 1
+while (c<=5){
+    console.log(c)
+    c++
+}
+*/
+
+// USANDO O FOR :
+
+console.log('Vai começar!')
+for (var c=1; c <= 5; c++){
+    console.log(c)
+}
+console.log('Fim do programa!')
+
