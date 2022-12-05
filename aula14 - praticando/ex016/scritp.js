7aa348adb788cfaf7645d8ea67a8cb696d1cdbde
Tiago <116377137+xeytak@users.noreply.github.com>
2022-12-05 10:52:15 -0300

    exercicios while

---
+++ aula14 - praticando/ex016/scritp.js
@@ -0,0 +1,16 @@
+/*
+function contar(){
+    var inicio = window.document.getElementById('textinicio')
+    var fim = window.document.getElementById('txtfim')
+    var passo = window.document.getElementById('txtpasso')
+    var contar = inicio + passo
+}*/
+
+var inicio = 2
+var fim = 20
+var passo = 2
+
+while (passo < fim){
+    passo += inicio
+    console.log(passo)
+}
